34f51e4859663312ce29a557abbb06e6215170eb
jfsancho <javiersanchomarin@hotmail.com>
2015-05-22 04:20:20 -0600

    Clase constants carga archivo xml Correcciones en Genetico

--- Midgard/logicFacade/LogicFacade.cpp
+++ Midgard/logicFacade/LogicFacade.cpp
@@ -10,11 +10,9 @@ LogicFacade::LogicFacade()
 
 void LogicFacade::runLogic(MainLogic* pMainLogic)
 {
-    _MainLogic = pMainLogic;    
-    _socketServer = new SocketServer();    
+    _MainLogic = pMainLogic;
+    _socketServer = new SocketServer();
 
-    //receiveDataFromSocket("{\"type\":\"1\"}");
-//{"type":"1"}
 }
 
 /**
@@ -35,15 +33,14 @@ void LogicFacade::getMap()
 {
 
     jsonWriterCpp* crearJson = new jsonWriterCpp();
-
+    char pArreglo[10000];
     //crearJson->writeMap(_MainLogic->getMap(),pArreglo);
 
-    cout << "llegoGetMap" << endl;    
+    cout << "llegoGetMap" << endl;
+    crearJson->writeMap(_MainLogic->getMap());
     cout << "manda mapa" << endl;
-    cout<< "height: " << _MainLogic->getMap()->getHeight()<< endl;
-    cout<< "width: " << _MainLogic->getMap()->getWidth()<< endl;
-
-    _socketServer->setMensaje(crearJson->writeMap((*_MainLogic->getMap())).c_str());
+    cout << "json "<< pArreglo << endl;
+    _socketServer->setMensaje(pArreglo);
 }
 
 /**
@@ -56,11 +53,11 @@ void LogicFacade::getGenealogia(Vector<int> pDatos)
      jsonWriterCpp* crearJson = new jsonWriterCpp();
      Vector<int>* padres = _MainLogic->getParents(pDatos[Raza],pDatos[individuoID]);
 
-
+     char pArreglo[10000];
      //crearJson->writeFamily((*padres)[Padre],(*padres)[Madre], (*padres)[indvFitness],pArreglo); // Le agrega el string que contiene el ID de ambos padres de pID
-     crearJson->writeFamily((*padres)[Padre],(*padres)[Madre], (*padres)[indvFitness]); // Le agrega el string que contiene el ID de ambos padres de pID
+     crearJson->writeFamily(*((*padres)[Padre]),*((*padres)[Madre]), *((*padres)[indvFitness])); // Le agrega el string que contiene el ID de ambos padres de pID
      cout << "creo el json en family" << endl;
-    //_socketServer->setMensaje(pArreglo);
+    _socketServer->setMensaje(pArreglo);
 }
 
 
