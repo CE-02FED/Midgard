b58a5988b28cd43e6f8bcf26cbd7d8566d0f4d05
Ernesto Ulate <netou18@gmail.com>
2015-05-26 03:47:51 -0600

    Conexión con Arduino establecida. Creación de números aleatorios. Actualización de Sketch Arduino.

--- RNG_LCD/RNG_LCD.ino
+++ RNG_LCD/RNG_LCD.ino
@@ -1,21 +1,27 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12,11,5,4,3,2);
 
-#define BAUD_RATE 9600
+#define BAUD_RATE 19200
 #define ANALOG_PIN 0
 #define LARGO 16
 #define ANCHO 2
+#define BYTE 256
 
 //Variables globales
 bool enviar = false;
+unsigned int binarios[BYTE];
+bool inicializar = true;
+unsigned int contadorCalibracion = 0;
 
 void setup() {
-  //Inicializar LCD
+  //Inicializar lcd
   lcd.begin(LARGO,ANCHO);
   printInicio();
   
   //Inicializa Serial
   Serial.begin(BAUD_RATE);
+  //Serial.flush();
+  //Serial.println("EC");
 }
 
 //Imprime esperando conexion
@@ -29,25 +35,22 @@ void printInicio(){
 
 
 void loop() {
+  byte _tendencia;
   int _valorPin = analogRead(ANALOG_PIN);
+  byte _valorByte = _valorPin >> 2;
+  bool _byteProcesado;
   
-  static int _contador = 0;
-  static byte _salida = 0;
-  if(_valorPin) {
-    _salida = (_salida << 1) | 0x01;
+  if (contadorCalibracion >= 1000) {
+    _tendencia = encontrarTendencia();
+    inicializar = false;
+  }
+  if (inicializar) {
+    calibrar(_valorByte);
+    contadorCalibracion++;
   }
   else {
-    _salida = (_salida << 1);
-  }
-  
-  _contador++;
-  _contador %= 8;
-  if(_contador == 0) {
-    if(enviar) {
-      Serial.println(_salida);
-      lcd.print(_salida);
-    }
-    _salida = 0;
+    _byteProcesado = (_valorByte < _tendencia) ? 1:0;
+    generarRandom(_byteProcesado);
   }
   
   if(!Serial.available()){  //Esperando conexion serial
@@ -55,11 +58,8 @@ void loop() {
   } 
   else {
     String info = readSerial();
-    if(info == "a"){
+    if(info == "check"){
       enviar = true;
-    } 
-    else {
-      Serial.flush();
     }     
   }
 }
@@ -70,4 +70,69 @@ String readSerial(){
     data += Serial.readString();
   }
   return data;
+}
+
+/*********************************************************/
+//Generar numero con bits
+void generarRandom(byte pValor) {
+  static bool _previo = 0;
+  static bool _variar = 0;
+  
+  _variar = !_variar;
+  if (_variar) {
+    if(pValor == 1 && _previo == 0) {
+      construirRandom(0);
+    }
+    else if (pValor == 0 && _previo == 1) {
+      construirRandom(1);
+    }
+  }
+  _previo = pValor;
+}
+
+void construirRandom(bool pValor) {
+  static int _contadorByte = 0;
+  static byte _salida = 0;
+  
+  if (pValor == 1) {
+    _salida = (_salida << 1) | 0x01;
+  }
+  else {
+    _salida = _salida << 1;
+  }
+  
+  _contadorByte++;
+  _contadorByte %= 8;
+  if (_contadorByte == 0) {
+    if(enviar) {
+      String _salidaStr = String(_salida);
+      _salidaStr += "#";
+      Serial.println(_salidaStr);
+      lcd.print(_salidaStr);
+    }
+    _salida = 0;
+  }
+}
+
+//Calibrar generador
+void calibrar(byte pValor) {
+  binarios[pValor]++;
+}
+
+unsigned int encontrarTendencia() {
+  unsigned long _mitad;
+  unsigned long _total = 0;
+  int _tendencia;
+  
+  for (_tendencia = 0; _tendencia < 256; _tendencia++) {
+    _total += binarios[_tendencia];
+  }
+  
+  _mitad = _total >> 1;
+  _total = 0;
+  for (_tendencia = 0; _tendencia < 256; _tendencia++) {
+    _total += binarios[_tendencia];
+    if (_total > _mitad) break;
+  }
+  return _tendencia;
 }
